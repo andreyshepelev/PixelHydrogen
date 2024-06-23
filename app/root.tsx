f8ced536793415cf81082d7f4b482f9861b9a0a8
Andrey Shepelev <andrei.s@pebblepost.com>
2024-06-23 13:20:58 +0300

    add test event tracking

--- app/root.tsx
+++ app/root.tsx
@@ -15,6 +15,7 @@ import favicon from '~/assets/favicon.svg';
 import resetStyles from '~/styles/reset.css?url';
 import appStyles from '~/styles/app.css?url';
 import {PageLayout} from '~/components/PageLayout';
+import {PixelAnalyticsIntegration} from '~/components/PixelAnalyticsIntegration';
 import {FOOTER_QUERY, HEADER_QUERY} from '~/lib/fragments';
 
 export type RootLoader = typeof loader;
@@ -157,6 +158,7 @@ function Layout({children}: {children?: React.ReactNode}) {
             consent={data.consent}
           >
             <PageLayout {...data}>{children}</PageLayout>
+            <PixelAnalyticsIntegration />
           </Analytics.Provider>
         ) : (
           children
